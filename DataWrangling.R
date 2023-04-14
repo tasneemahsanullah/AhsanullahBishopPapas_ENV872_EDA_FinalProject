1988942760b92ab1989b76caeb56d2e65cae151a
Ana Bishop <cb32@email.sc.edu>
2023-04-14 14:52:33 -0400

    cleaned dataset!

--- DataWrangling.R
+++ DataWrangling.R
@@ -1,5 +1,6 @@
 #load libraries
 library(tidyverse)
+library(lubridate)
 
 #check wd
 getwd()
@@ -7,3 +8,10 @@ getwd()
 #load csvs
 points <- read_csv("./Data/whaleshark_data/dataset_points.csv")
 citation <- read_csv("./Data/whaleshark_data/dataset_citation.csv")
+
+
+gom <- points %>% filter(latitude >= 17 & latitude <= 30) %>% filter (longitude >= -98 & longitude <= -79)
+
+gom_clean <- gom %>% separate(date_time, into = c('date','time'), sep=" ")
+
+gom_clean$date <- ymd(gom_clean$date)
